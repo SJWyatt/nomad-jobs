505c372daf1d453ec89a195721010438d746a732
Xavier Merino <xaviermerino@gmail.com>
2020-04-10 00:30:39 -0400

    Updating data to match grafana map

---
+++ demo-covid19-us/standalone/ingest.sql
@@ -0,0 +1,34 @@
+SET GLOBAL local_infile = 'ON';
+
+CREATE TABLE bases( 
+name varchar(255), 
+state varchar(255), 
+latitude float(10,6), 
+longitude float(10,6)
+);
+
+-- Importing the csv file data in the tables createsLOAD DATA LOCAL 
+
+LOAD DATA LOCAL INFILE '/home/ubuntu/sql/af_bases.csv' INTO TABLE bases 
+FIELDS TERMINATED BY ',' ENCLOSED BY '' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS;
+
+-- SET GLOBAL local_infile = 'ON';
+
+CREATE TABLE bases( 
+name varchar(255), 
+state varchar(255), 
+latitude float(10,6), 
+longitude float(10,6),
+value float not null default '1.00',
+timestamp timestamp not null default current_timestamp on update current_timestamp
+);
+
+-- -- Importing the csv file data in the tables createsLOAD DATA LOCAL 
+
+LOAD DATA LOCAL INFILE '/home/ubuntu/sql/af_bases.csv' INTO TABLE bases 
+FIELDS TERMINATED BY ',' ENCLOSED BY '' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS;
+
