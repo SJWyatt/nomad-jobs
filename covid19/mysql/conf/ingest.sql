99373028518a59dbc5d6a416c422fbc2ff21e88b
Xavier Merino <xaviermerino@gmail.com>
2020-04-12 01:40:17 -0400

    Applying a fix

--- covid19/mysql/conf/ingest.sql
+++ covid19/mysql/conf/ingest.sql
@@ -4,10 +4,11 @@ CREATE TABLE bases(
 name varchar(255), 
 state varchar(255), 
 geohash varchar(255),
-confirmed int,
-timestamp timestamp not null default current_timestamp on update current_timestamp,
+confirmed float,
+timestamp timestamp not null default current_timestamp on update current_timestamp
 );
 
-LOAD DATA INFILE '/local/data/af_bases.csv' INTO TABLE bases 
-FIELDS TERMINATED BY ',' ENCLOSED BY '' 
-LINES TERMINATED BY '\n';
+LOAD DATA INFILE '/local/data/af_bases.csv' 
+INTO TABLE bases (name, state, geohash, confirmed)
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'; 
