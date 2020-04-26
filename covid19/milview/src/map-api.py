0e10ab6190d4845570442a5ffe3fd994514ddf33
Xavier Merino <xaviermerino@gmail.com>
2020-04-25 20:03:19 -0400

    Updates to military view.

--- covid19/milview/src/map-api.py
+++ covid19/milview/src/map-api.py
@@ -12,8 +12,8 @@ import traceback
 import json
 from mapquery import MapQuery
 
-INFLUX_HOST = os.environ['INFLUX_HOST']
-INFLUX_PORT =  os.environ['INFLUX_DBPORT']
+INFLUX_HOST = '172.31.80.76' # os.environ['INFLUX_HOST']
+INFLUX_PORT =  8086 # os.environ['INFLUX_DBPORT']
 military_view = MapQuery(INFLUX_HOST, INFLUX_PORT)
 
 class Server_Check:
@@ -44,56 +44,17 @@ class Query:
         print("--------------------------------------------------------------------------------------")
         print("/Query Endpoint...")
         military_data = {}
+
         try:
-            # print("Request: ",request)
             data = json.load(req.bounded_stream)
             print("JSON Request:", data)
 
-            # fromDate = data['range']['from']
-            # toDate = data['range']['to']
-            # interval = data['intervalMs']
-            # maxPoints = data['maxDataPoints']
-
             scopedVars = data.get('scopedVars', {})
-            state = scopedVars.get('States', {})
-            county = scopedVars.get('County', {})
-
-            states = state.get('value', 'All')
-            counties = county.get('value', 'All')
-
-            
-            if state.get('text', 'All') == 'All':
-                states = 'All'
-
-            if county.get('text', 'All') == 'All':
-                counties = None
-
-            # targets = data['targets']
-            
-            # print("Get data from %s to %s"%(fromDate, toDate))
-            # print("%d number of points at %dms intervals."%(maxPoints, interval))
-            print("States:", states, "Counties:",counties)
-            # print("Targets",targets)
-
-            # stats = get_data(fromDate, toDate, maxPoints, interval, states, counties, targets)
-
-            # ignore what the user says they want, we're giving them what we want to.
-            # military_data = [{
-            #     # 'name':"Covid19 Military View",
-            #     "columns":[
-            #         "time",
-            #         "Confirmed",
-            #         "geohash",
-            #         "location",
-            #         "state"
-            #     ],
-            #     "rows": [],
-            #     "type":"table"
-            # }]
-            # military_data[0]['rows'] = military_view.get_military_table_output()
+            bases = scopedVars.get('Base', {})
+            bases = bases.get('value')
+            range_to = data.get('range', {}).get('to')
 
             military_data = [{
-                # 'name':"Covid19 Military View",
                 "columns":[
                     {"text":"time","type":"time"},
                     {"text":"Confirmed","type":"number"},
@@ -104,7 +65,7 @@ class Query:
                 "rows": [],
                 "type":"table"
             }]
-            military_data[0]['rows'] = military_view.get_military_table_output()
+            military_data[0]['rows'] = military_view.get_military_table_output(bases, range_to)
 
         except Exception as e:
             print("Query Exception:", e)
